7a56a6cc65047cd2da2d658c12b017f6b726ee5c
Statz Tatsumi Saines <statsumi.sts@gmail.com>
2023-10-19 18:30:57 -0400

    final product

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Write your solution in this file!
+//1st test
+const employee = {name: 'Sam'}
+const streetAddress = '11 Broadway'
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    const mondayEmployee = {...employee}
+    mondayEmployee[key] = value 
+    return mondayEmployee
+}
+//3rd test
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value
+    return employee
+    const mondayAddress = destructivelyUpdateEmployeeWithKeyAndValue(employee, streetAddress)
+    mondayAddress.streetAddress = '12 Broadway'
+    return mondayAddress
+}
+//4th test
+function deleteFromEmployeeByKey(employee, key) {
+    const newEmployee = {...employee}
+    delete newEmployee[key]
+    return newEmployee
+}
+//6th test
+function destructivelyDeleteFromEmployeeByKey(employee, key, value) {
+    employee[key] = value
+    return employee
+    const tuesdayEmployee = destructivelyDeleteFromEmployeeByKey(employee, key)
+    delete tuesdayEmployee[key]
+    return tuesdayEmployee
+}
